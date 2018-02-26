8f70f3ebc9d15ff61a96022ea5a859282967feb9
Eli Meschures <meschures.eli@gmail.com>
2018-02-26 12:40:57 -0800

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
+  delete '/logout' => 'sessions#destroy', as: :logout
   resources :users
-
+  resources :sessions, only: [:new, :create]
 end
